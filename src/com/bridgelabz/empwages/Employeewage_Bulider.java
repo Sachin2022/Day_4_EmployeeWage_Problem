2e588df901880f0c30e069f268575c4e0ae1690f
Textspot <sachinkatarki1995@gmail.com>
2021-12-23 14:24:19 +0530

    Added:Using the Switch Case staetment solving the Employee wage

--- src/com/bridgelabz/empwages/Employeewage_Bulider.java
+++ src/com/bridgelabz/empwages/Employeewage_Bulider.java
@@ -10,23 +10,19 @@ public class Employeewage_Bulider {
 		int wage_pr_hr=20;
 		int working_hr;
 		int emp_check=(int)(Math.floor(Math.random()*10))%3;
-		if(emp_check==is_fulltime_present) 
-		{
-			System.out.println("Employee is full time Present");
-			working_hr=8;
+		switch(emp_check) {
+		     case 1:
+		    	 System.out.println("Employee is full time present");
+		    	 working_hr=8;
+		    	 
+		     case 2:
+		    	 System.out.println("Employee is part time present");
+		    	 working_hr=4;
+		    	 
+		     default:
+		    	 System.out.println("Employee is Absent");
+		    	 working_hr=0;
 		}
-		else if(emp_check==is_parttime_present)
-		{
-			System.out.println("Employee is Present");
-			working_hr=4;
-		}
-		else 
-		{
-			
-			System.out.println("Employee is Abesent");
-		working_hr=0;
-	   }
-	
        daily_wage=working_hr * wage_pr_hr;
        System.out.println("Employee daily wage is" + daily_wage);
 			
