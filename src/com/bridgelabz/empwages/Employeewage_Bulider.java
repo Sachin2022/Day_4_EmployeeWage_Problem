4d8f9846335a0ef54d622dd5eac12785c9d175e7
Textspot <sachinkatarki1995@gmail.com>
2021-12-23 13:41:39 +0530

    Added:Added the Employee Parttime and calculate the Employee wage

--- src/com/bridgelabz/empwages/Employeewage_Bulider.java
+++ src/com/bridgelabz/empwages/Employeewage_Bulider.java
@@ -4,15 +4,21 @@ public class Employeewage_Bulider {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to EmployeeWage Programme");
-		int is_present=1;
+		int is_fulltime_present=1;
+		int is_parttime_present=2;
 		int daily_wage;
 		int wage_pr_hr=20;
 		int working_hr;
-		int emp_check=(int)(Math.floor(Math.random()*10))%2;
-		if(emp_check==is_present) 
+		int emp_check=(int)(Math.floor(Math.random()*10))%3;
+		if(emp_check==is_fulltime_present) 
+		{
+			System.out.println("Employee is full time Present");
+			working_hr=8;
+		}
+		else if(emp_check==is_parttime_present)
 		{
 			System.out.println("Employee is Present");
-			working_hr=8;
+			working_hr=4;
 		}
 		else 
 		{
