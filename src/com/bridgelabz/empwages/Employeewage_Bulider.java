ca59901fb46cdba54a28029aa49a2a6f78530346
Textspot <sachinkatarki1995@gmail.com>
2021-12-23 13:02:35 +0530

    Added:To Randomly check the Employee is Present or Absent

--- src/com/bridgelabz/empwages/Employeewage_Bulider.java
+++ src/com/bridgelabz/empwages/Employeewage_Bulider.java
@@ -4,7 +4,17 @@ public class Employeewage_Bulider {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to EmployeeWage Programme");
+		int is_present=1;
+		int emp_check=(int)(Math.floor(Math.random()*10))%2;
+		if(emp_check==is_present) 
+		{
+			System.out.println("Employee is Present");
+		}
+		else 
+			
+			System.out.println("Employee is Abesent");
+			
+	      }
+	 }
 
-	}
 
-}
