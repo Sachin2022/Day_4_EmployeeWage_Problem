6501d5ab7047785783808eef55ff05e09d21e5f5
Textspot <sachinkatarki1995@gmail.com>
2021-12-23 12:50:23 +0530

    Added:Welcome to the EmployeeWage Programme

---
+++ src/com/bridgelabz/empwages/Employeewage_Bulider.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.empwages;
+
+public class Employeewage_Bulider {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage Programme");
+
+	}
+
+}
