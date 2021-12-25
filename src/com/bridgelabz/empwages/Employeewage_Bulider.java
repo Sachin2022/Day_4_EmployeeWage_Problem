74727f05715d00d8dfc62076ed1c91a3dda89681
Sachin2022 <96484498+Sachin2022@users.noreply.github.com>
2021-12-25 12:57:08 +0530

    Update Employeewage_Bulider.java

--- src/com/bridgelabz/empwages/Employeewage_Bulider.java
+++ src/com/bridgelabz/empwages/Employeewage_Bulider.java
@@ -12,25 +12,25 @@ public class Employeewage_Bulider {
 		int emp_check=(int)(Math.floor(Math.random()*10))%3;
 		if(emp_check==is_fulltime_present) 
 		{
-			System.out.println("Employee is full time Present");
-			working_hr=8;
+		       System.out.println("Employee is full time Present");
+		       working_hr=8;
 		}
 		else if(emp_check==is_parttime_present)
 		{
-			System.out.println("Employee is Present");
-			working_hr=4;
+		      System.out.println("Employee is Present");
+		      working_hr=4;
 		}
 		else 
 		{
 			
-			System.out.println("Employee is Abesent");
-		working_hr=0;
-	   }
+		     System.out.println("Employee is Abesent");
+		     working_hr=0;
+	        }
 	
-       daily_wage=working_hr * wage_pr_hr;
-       System.out.println("Employee daily wage is" + daily_wage);
+                     daily_wage=working_hr * wage_pr_hr;
+                     System.out.println("Employee daily wage is" + daily_wage);
+	        }
 			
-	      }
-	 }
+	     }
 
 
