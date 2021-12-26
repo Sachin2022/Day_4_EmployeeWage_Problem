d046a2bda66bd26f2583a96f493786bdf122b477
Sachin2022 <96484498+Sachin2022@users.noreply.github.com>
2021-12-26 10:06:43 +0530

    Update Employeewage_Bulider.java

--- src/com/bridgelabz/empwages/Employeewage_Bulider.java
+++ src/com/bridgelabz/empwages/Employeewage_Bulider.java
@@ -16,30 +16,27 @@ public class Employeewage_Bulider {
 		int emp_check=(int)(Math.floor(Math.random()*10))%3;
 		for (int i=0; i<days_in_month && total_working_hr<max_working_hrs; i++) {
 		switch(emp_check) {
-		     case 1:
+		     case 1: working_hr=8;
 		    	 
-		    	 working_hr=8;
-		    	 break;
-		    	 
-		     case 2:
+		     break;	
 		    	
-		    	 working_hr=4;
-		    	 break;
+		     case 2:working_hr=4;
+		    	
+		     break;	 
 		    	 
-		     default:
+		     default: working_hr=0;
 		    	 
-		    	 working_hr=0;
 		}
-		total_working_hr += working_hr;
-		daily_wage_array[i]=working_hr * wage_pr_hr;
+		     total_working_hr += working_hr;
+		     daily_wage_array[i]=working_hr * wage_pr_hr;
 		
 		}
-		for (int j = 0; j < days_in_month; j++) {
+		     for (int j = 0; j < days_in_month; j++) {
 			int day = j + 1;
 			System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
 			monthly_total_wage = monthly_total_wage + daily_wage_array[j];
 		}
-		System.out.println("\nMonthly wage is " + monthly_total_wage);
+		     System.out.println("\nMonthly wage is " + monthly_total_wage);
 	   
    	    }
        
